13f67654ed5de5f71c77db6d7cce189bce08cfa0
Meghan Paul <mlpaul4901@gmail.com>
2022-03-30 21:04:53 -0500

    filtering in the get product route

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -3,29 +3,50 @@ const withAuth = require('../utils/auth');
 
 //Theorhetical spaceholders for product model
 const Products = require('../../models');
+const sequelize = require('sequelize');
 
 //GET for the products homepage
 router.get('/:filter', async (req, res) => {
+
+    //theorhetical attribute names
+    const attributes = [
+        'img_link',
+        'title',
+        'description',
+        'price',
+        'user_id',
+        'votes',
+        'creation_time'
+    ];
+
     try
     {
-        const dbProductData = await Products.findAll({
-            include:
-            [
-                {
-                    model: Products,
-                    //theorhetical attribute names
-                    attributes: [
-                        'img_link',
-                        'title',
-                        'description',
-                        'price',
-                        'user_id',
-                        'votes',
-                        'creation_time'
-                    ]
-                },
-            ],
-        });
+        if(filter=='sortByVotes')
+        {
+            const dbProductData = await Products.findAll({
+                include:
+                [
+                    {
+                        model: Products,
+                        attributes: attributes,
+                        order: sequelize.literal('votes','DESC')
+                    },
+                ]
+            });
+        }else if(filter=='sortByRecent'){
+            const dbProductData = await Products.findAll({
+                include:
+                [
+                    {
+                        model: Products,
+                        attributes: attributes,
+                        order: sequelize.literal('creation_time','DESC')
+                    }
+                ]
+            });
+        }else{
+            throw new Error('Invalid Route');
+        }
 
         const productPage = dbProductData.map((product)=> {
             product.get({plain:true});
@@ -34,7 +55,6 @@ router.get('/:filter', async (req, res) => {
         res.render('home', {
             productPage,
             loggedIn: req.session.loggedIn,
-            filterValue: req.params.filter,
         });
     }catch (err) {
         console.log(err);
@@ -42,3 +62,4 @@ router.get('/:filter', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
