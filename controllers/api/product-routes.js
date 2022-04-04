f83be9a36063795e815542cf58ad59cf7a3ab764
Skronkie <ebrockway01@gmail.com>
2022-04-04 14:25:31 -0500

    commented out old get route for now, added one to display products on homepage without filter functionality

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -6,57 +6,65 @@ const { Product } = require("../../models");
 const sequelize = require("sequelize");
 
 //GET for the products homepage
-router.get("/:filter", async (req, res) => {
-  //theorhetical attribute names
-  const attributes = [
-    "img_link",
-    "title",
-    "description",
-    "price",
-    "user_id",
-    "votes",
-    "creation_time",
-  ];
-
-  try {
-    if (filter == "sortByVotes") {
-      const dbProductData = await Products.findAll({
-        include: [
-          {
-            model: Products,
-            attributes: attributes,
-            order: sequelize.literal("votes", "DESC"),
-          },
-        ],
-      });
-    } else if (filter == "sortByRecent") {
-      const dbProductData = await Products.findAll({
-        include: [
-          {
-            model: Products,
-            attributes: attributes,
-            order: sequelize.literal("creation_time", "DESC"),
-          },
-        ],
-      });
-    } else {
-      throw new Error("Invalid Route");
-    }
-
-    const productPage = dbProductData.map((product) => {
-      product.get({ plain: true });
+router.get("/", (req, res) => {
+  Product.findAll({
+    attributes: ["id", "title", "description", "price", "img_link"],
+  })
+    .then((dbProductData) => {
+      const products = dbProductData.map((product) =>
+        product.get({ plain: true })
+      );
+      res.render("home", { products });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-
-    res.render("home", {
-      productPage,
-      loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    //status 500 -> server error, server doesn't know how to handle the route
-    res.status(500).json(err);
-  }
 });
+
+// router.get("/:filter", async (req, res) => {
+//   //theorhetical attribute names
+//   const attributes = ["img_link", "title", "description", "price", "user_id"];
+
+//   try {
+//     if (req.params.filter == "sortByVotes") {
+//       const dbProductData = await Product.findAll({
+//         include: [
+//           {
+//             model: Product,
+//             attributes: attributes,
+//             order: sequelize.literal("votes", "DESC"),
+//           },
+//         ],
+//       });
+//     } else if (req.params.filter == "sortByRecent") {
+//       const dbProductData = await Product.findAll({
+//         include: [
+//           {
+//             model: Products,
+//             attributes: attributes,
+//             order: sequelize.literal("creation_time", "DESC"),
+//           },
+//         ],
+//       });
+//     } else {
+//       throw new Error("Invalid Route");
+//     }
+
+//     const productPage = dbProductData.map((product) => {
+//       product.get({ plain: true });
+//     });
+
+//     res.render("home", {
+//       productPage,
+//       loggedIn: req.session.loggedIn,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     //status 500 -> server error, server doesn't know how to handle the route
+//     res.status(500).json(err);
+//   }
+// });
 
 router.post("/", (req, res) => {
   Product.create({
