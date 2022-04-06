4c262171816b9cf82dbc30c492dd4ddd31b9ece6
Skronkie <ebrockway01@gmail.com>
2022-04-06 18:23:04 -0500

    added display for products on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,16 +1,37 @@
-const router = require('express').Router();
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const withAuth = require("../utils/auth");
+const { Product, Comment } = require("../models");
 
-router.get('/', withAuth, (req, res) => {
-  res.render('home', { user: req.session.user });
+router.get("/", (req, res) => {
+  Product.findAll({
+    attributes: ["id", "title", "description", "price", "img_link"],
+    include: {
+      model: Comment,
+      attributes: ["id", "text", "user_id", "product_id", "created_at"],
+    },
+  })
+    .then((dbProductData) => {
+      const products = dbProductData.map((product) =>
+        product.get({ plain: true })
+      );
+
+      res.render("home", { products });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+router.get("/", withAuth, (req, res) => {
+  res.render("home", { user: req.session.user });
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
