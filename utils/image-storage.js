30f8c6c6e3af36c3efd0094da44796b82e2d4167
Meghan Paul <mlpaul4901@gmail.com>
2022-04-10 22:52:34 -0500

    ref._throwIfRoot is not a function error ln:35 image-storage.js

--- utils/image-storage.js
+++ utils/image-storage.js
@@ -1,36 +1,44 @@
 import dotenv from "dotenv";
 dotenv.config();
-import {initializeApp} from "firebase/app";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-  uploadBytes,
-} from "firebase/storage";
+import {initializeApp} from "firebase-admin/app";
+import { getStorage } from 'firebase-admin/storage'
+import {ref, uploadBytes} from 'firebase/storage';
+import uuid from 'uuid-v4';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  storageBucket: process.env.FIREBASE_STORAGE_URL,
+  apiKey: "AIzaSyDteDKsKaq2Z_C8sbY-HXwVcTW3O-7uDWs",
+  authDomain: "shop-45864.firebaseapp.com",
+  projectId: "shop-45864",
+  storageBucket: "shop-45864.appspot.com",
+  messagingSenderId: "58636220724",
+  appId: "1:58636220724:web:37b3b0ad321b6fd866cd61",
+  measurementId: "G-D40GD0RVCW"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 const storage = getStorage(firebaseApp);
+const storageRef = ref(storage);
 
 export async function uploadImg(user, title, file) {
-  const filePath = `${user}/${title}`;
-  const imgRef = ref(storage, filePath);
+  title = title.replace(/[^a-zA-Z0-9 ]/g,'');
+  const filePath = '/User ' + user + ' ' + title;
+  console.log('File Path: ' + filePath);
+  console.log(storageRef);
+  const imgRef = ref(storageRef, filePath);
+  const token = uuid();
+  const metadata = {
+    firebaseStorageDownloadTokens: token
+  };
   //const uploadTask = uploadBytesResumable(imgRef, file);
   console.log("firebase file path: " + filePath);
 
-  uploadBytes(imgRef, file).then((snapshot)=> {
+ uploadBytes(imgRef, file, metadata).then((snapshot)=> {
     console.log('Uploaded file');
     console.log(snapshot);
   });
 
   const downloadURL = new Promise((res,rej) => {
-    res(getDownloadUrl(uploadTask.snapshot.ref));
+    res(getDownloadUrl(process.env.FIREBASE_AUTH_DOMAIN,filename,token));
   });
 
   downloadURL
@@ -38,10 +46,10 @@ export async function uploadImg(user, title, file) {
     console.log("File stored at " + url);
     return(url);
   });
-
-  /* 
+}
+   
     //#1 attempt from Firebase docs
-    uploadTask.on(
+    /* uploadTask.on(
     "state_changed",
     (snapshot) => {
       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
@@ -75,7 +83,7 @@ export async function uploadImg(user, title, file) {
     () => {
       //Upload successful
       const downloadURL = new Promise((res,rej) => {
-        res(getDownloadUrl(uploadTask.snapshot.ref));
+        res(getDownloadUrl(process.env.FIREBASE_AUTH_DOMAIN,));
       });
 
       downloadURL
@@ -84,8 +92,11 @@ export async function uploadImg(user, title, file) {
         return(downloadURL);
       });
     }
-  );*/
-}
+  );
+} */
 
-/* <label for='img'>Select Image to Upload</label>
-<input type='file' id='img' name='image' accept='image/*' capture='environment'/> */
+const getDownloadUrl = (bucket, pathToFile, downloadToken) => {
+  return `https://firebasestorage.googleapis.com/v0/b/${bucket}/o/${encodeURIComponent(
+    pathToFile
+  )}?alt=media&token=${downloadToken}`;
+}
